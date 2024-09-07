ae3856e9dce30b5f1388b337e5fd1e2743e3845f
Vijendra Kumar Vaishya <78156586+vennyvirtuoso@users.noreply.github.com>
2024-09-07 10:34:23 +0530

    f

--- app.py
+++ app.py
@@ -5,14 +5,18 @@ import math
 from dotenv import load_dotenv
 import os
 
-POS_TAGGER_MODEL_FILE = "pos-tagger.pkl"
+POS_TAGGER_MODEL_FILE = "pos-tagger-1smth.pkl"
+
+with open(POS_TAGGER_MODEL_FILE, "rb") as f:
+    model = dill.load(f)
+
 from collections import defaultdict
-def hmm_pos_tagger(sentence):
-    with open(POS_TAGGER_MODEL_FILE, "rb") as f:
-        model = dill.load(f)
-        return [tag for word, tag in list(model.predict(sentence))]
+def hmm_pos_tagger(sentence):    
+    return [tag for word, tag in model.predict(sentence)]
 
 # Set OpenAI API key
+load_dotenv()
+# openai.api_key = os.environ.get("OPENAI_API_KEY")
 openai.api_key = st.secrets["OPENAI_API_KEY"]
 
 def gpt4_pos_tagger(sentence):
@@ -28,7 +32,7 @@ def gpt4_pos_tagger(sentence):
     response = openai.ChatCompletion.create(
         model="gpt-4",
         messages=[
-            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "system", "content": "You are a part-of-speech (POS) tagger."},
             {"role": "user", "content": prompt}
         ]
     )
@@ -53,7 +57,10 @@ def create_comparison_table(sentence_words, gpt4_tags, hmm_tags):
     table += "<tr><th style='border: 1px solid black; padding: 8px; text-align: left;'>HMM POS Tagger</th>"
     ind = 0
     for tag in hmm_tags:
-        color = "green" if tag.strip() == gpt4_tags[ind].strip() else "red"
+        if ind < len(gpt4_tags):
+            color = "green" if tag.strip() == gpt4_tags[ind].strip() else "red"
+        else:
+            color = "red"
         table += f"<td style='border: 1px solid black; padding: 8px;'><span style=\"color:{color}\">{tag}</span></td>"
         ind += 1
     table += "</tr>"
@@ -62,20 +69,21 @@ def create_comparison_table(sentence_words, gpt4_tags, hmm_tags):
     
     return table
 
-st.set_page_config(page_title="POS Tagger Demo", page_icon="📊", layout="wide")
+st.set_page_config(page_title="POS Tagger Demo0", page_icon="📊", layout="wide")
 st.markdown("<h1 style='text-align: center;'>POS Tagger Comparison: HMM vs GPT-4</h1>", unsafe_allow_html=True)
 
 sentence = st.text_area("Sentence Input", height=50, placeholder="Type your sentence here...", label_visibility="collapsed")
 
 if st.button("Submit", use_container_width=True):
     if sentence.strip():
-        sentence_words = sentence.split()
+        sentence_words = model.tokenizer(sentence)
         gpt4_tags = gpt4_pos_tagger(sentence)
         hmm_tags = hmm_pos_tagger(sentence)
         for i in range(len(gpt4_tags)):
             actual_tags = ''.join([c for c in gpt4_tags[i] if c != "'"])
             gpt4_tags[i] = actual_tags
-    
+        # print(gpt4_tags)
+        # print(hmm_tags)
         comparison_table = create_comparison_table(sentence_words, gpt4_tags, hmm_tags)
     
         st.write("### POS Tagging Comparison")
